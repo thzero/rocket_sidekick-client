28dac3c59eac285206a321571ee3015908e621c5
thzero <thzero@users.noreply.github.com>
2023-04-14 07:27:57 -0500

    dependency updates

--- src/service.app/utility.js
+++ src/service.app/utility.js
@@ -1,3 +1,5 @@
+import LibraryClientConstants from '@thzero/library_client/constants.js';
+
 import AppUtilityService from '@/service/utility';
 
 class WebUtilityService extends AppUtilityService {
@@ -10,6 +12,21 @@ class WebUtilityService extends AppUtilityService {
 		})();
 
 		return response;
+	}
+
+	async contentReset(correlationId, contentId) {
+		try {
+			const body = {
+				contentId: contentId != null ? contentId : null
+			};
+
+			const response = await this._serviceCommunicationRest.post(correlationId, LibraryClientConstants.ExternalKeys.BACKEND, { url: 'utility/content/reset' }, body);
+			this._logger.debug('AppUtilityService', 'contentReset', 'response', response, correlationId);
+			return response;
+		}
+		catch (err) {
+			return this._error('AppUtilityService', 'contentReset', null, err, null, null, correlationId);
+		}
 	}
 }
 
