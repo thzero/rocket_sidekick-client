879ffe38d5b3cbf1c99d1763284c3e92dadff223
thzero <thzero@users.noreply.github.com>
2023-05-31 19:15:16 -0500

    updates

--- src/main.js
+++ src/main.js
@@ -12,7 +12,6 @@ import bootStarter from '@thzero/library_client_firebase/boot/starter';
 // import bootAsyncComputed from '@thzero/library_client_vue3/boot/asyncComputed';
 import bootEventBus from '@thzero/library_client_vue3/boot/eventBus';
 import booti18n from '@/boot/i18n';
-import bootNetwork from '@/boot/network';
 // import bootServices from '@/boot/services';
 import bootServices from '@/boot.app/services';
 import bootServicesAdmin from '@/boot.app/servicesAdmin';
@@ -29,9 +28,10 @@ import store from '@/store.app/pinia'; // STORE TYPE
 
 import start from '@thzero/library_client_vue3/boot/main';
 
-import App from '@/components/App.vue';
+// import App from '@/components/App.vue';
+import App from '@/components.app/App.vue';
 
-start(App, router, store, [ booti18n, bootEventBus, bootNetwork, bootServices, bootServicesAdmin, bootValidate, bootUi, bootCookieComply ], bootStarter, {
+start(App, router, store, [ booti18n, bootEventBus, bootServices, bootServicesAdmin, bootValidate, bootUi, bootCookieComply ], bootStarter, {
 	idGenerator: {
 		alphabet: AppSharedConstants.IdGenerator.alphabet,
 		lengthLong: AppSharedConstants.IdGenerator.lengthLong,
