5d2fc6a727266f2f8e0c0ebef3b16e364f965d98
Corvette Rox <development@thzero.com>
2024-12-23 09:30:56 -0600

    fix issues with updated devices

--- src/service.app/tools/flightInfo/processors/eggtimer.js
+++ src/service.app/tools/flightInfo/processors/eggtimer.js
@@ -49,21 +49,54 @@ class EggtimerFlightInfoProcessorService extends FlightInfoProcessorService {
 		const checkResponse = this._check(correlationId, input);
 		if (this._hasFailed(checkResponse))
 			return checkResponse;
+		
+		const colAltitude = input.data[0].findIndex(l => l === 'Alt');
+		const colAltitudeF = input.data[0].findIndex(l => l === 'FAlt');
+		const colApogee = input.data[0].findIndex(l => l === 'Apogee');
+		const colDrogue = input.data[0].findIndex(l => l === 'Drogue');
+		const colMain = input.data[0].findIndex(l => l === 'Main');
+		const colNoseOver = input.data[0].findIndex(l => l === 'N-O');
+		const colTime = input.data[0].findIndex(l => l === 'T');
+		const colVelocity = input.data[0].findIndex(l => l === 'Veloc');
+		const colVelocityF = input.data[0].findIndex(l => l === 'FVeloc');
+
+		let valid = true;
+		let errors = [];
+		valid &= this._checkField(correlationId, colAltitude, 'Altitude', errors);
+		valid &= this._checkField(correlationId, colAltitudeF, 'Altitude Filtered', errors);
+		valid &= this._checkField(correlationId, colApogee, 'Apogee', errors);
+		valid &= this._checkField(correlationId, colDrogue, 'Drogue', errors);
+		valid &= this._checkField(correlationId, colMain, 'Main', errors);
+		valid &= this._checkField(correlationId, colNoseOver, 'Nose Over', errors);
+		valid &= this._checkField(correlationId, colTime, 'Timestamp', errors);
+		valid &= this._checkField(correlationId, colVelocity, 'Velocity', errors);
+		valid &= this._checkField(correlationId, colVelocityF, 'Velocity Filtered', errors);
+		if (!valid)
+			return this._error('EggtimerFlightInfoProcessorService', '_processInput', 'Non Eggtimer Quantum file detected', errors, AppConstants.FlightInfo.Errors.NonQuantum, null, correlationId);
 
 		input.data.shift();
 
 		for (const data of input.data) {
 			this._publish(
 				correlationId,
-				data[0], // time
-				data[1], // altitude
-				data[3], // altitudeF
-				data[2], // velocity
-				data[4], // velocityF
-				data[7], // apogee
-				data[8], // noseOver
-				data[9], // drogue
-				data[10], // main
+				// data[0], // time
+				// data[1], // altitude
+				// data[3], // altitudeF
+				// data[2], // velocity
+				// data[4], // velocityF
+				// data[7], // apogee
+				// data[8], // noseOver
+				// data[9], // drogue
+				// data[10], // main
+				data[colTime], // time
+				data[colAltitude], // altitude
+				data[colAltitudeF], // altitudeF
+				data[colVelocity], // velocity
+				data[colVelocityF], // velocityF
+				data[colApogee], // apogee
+				data[colNoseOver], // noseOver
+				data[colDrogue], // drogue
+				data[colMain], // main
 				data.length < 1 // ground
 			);
 		}
